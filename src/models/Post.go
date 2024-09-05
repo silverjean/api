19db271a8ff1b7a32a3a9b0c9180d8f88d01ad62
Jean dos Santos Silveira <silveirajeans@gmail.com>
2024-09-04 21:57:11 -0300

    create a struct for the posts

---
+++ src/models/Post.go
@@ -0,0 +1,13 @@
+package models
+
+import "time"
+
+type Post struct {
+	ID         uint64    `json:"id,omitempty"`
+	Title      string    `json:"title,omitempty"`
+	Content    string    `json:"content,omitempty"`
+	AuthorID   uint64    `json:"authorId,omitempty"`
+	AuthorNick string    `json:"authorNick,omitempty"`
+	Likes      uint64    `json:"likes"`
+	CriateAt   time.Time `json:"createAt,omitempty"`
+}
